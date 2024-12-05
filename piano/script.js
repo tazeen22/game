bd537a736edf109cc7df417f28097becf5da1380
tazeen22 <tazeen.2229@gmail.com>
2024-12-05 11:36:14 +0530

    commit

--- piano/script.js
+++ piano/script.js
@@ -3,36 +3,33 @@ volumeSlider = document.querySelector(".volume-slider input"),
 keysCheckbox = document.querySelector(".keys-checkbox input");
 
 let allKeys = [],
-audio = new Audio(`tunes/a.wav`); // by default, audio src is "a" tune
+audio = new Audio(`tunes/a.wav`);
 
 const playTune = (key) => {
-    audio.src = `tunes/${key}.wav`; // passing audio src based on key pressed 
-    audio.play(); // playing audio
+    audio.src = `tunes/${key}.wav`;
+    audio.play();
 
-    const clickedKey = document.querySelector(`[data-key="${key}"]`); // getting clicked key element
-    clickedKey.classList.add("active"); // adding active class to the clicked key element
-    setTimeout(() => { // removing active class after 150 ms from the clicked key element
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active");
+    setTimeout(() => {
         clickedKey.classList.remove("active");
     }, 150);
 }
 
 pianoKeys.forEach(key => {
-    allKeys.push(key.dataset.key); // adding data-key value to the allKeys array
-    // calling playTune function with passing data-key value as an argument
+    allKeys.push(key.dataset.key);
     key.addEventListener("click", () => playTune(key.dataset.key));
 });
 
 const handleVolume = (e) => {
-    audio.volume = e.target.value; // passing the range slider value as an audio volume
+    audio.volume = e.target.value;
 }
 
 const showHideKeys = () => {
-    // toggling hide class from each key on the checkbox click
     pianoKeys.forEach(key => key.classList.toggle("hide"));
 }
 
 const pressedKey = (e) => {
-    // if the pressed key is in the allKeys array, only call the playTune function
     if(allKeys.includes(e.key)) playTune(e.key);
 }
 
